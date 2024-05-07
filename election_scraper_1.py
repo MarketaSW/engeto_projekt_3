0f98322f219e1d7c2c92020308a593ef580736d9
MarketaSW <marketa.wallo@gmail.com>
2024-05-07 11:26:29 +0200

    Define write_to_csv()

--- election_scraper_1.py
+++ election_scraper_1.py
@@ -1,5 +1,7 @@
 import requests
+import csv
 from bs4 import BeautifulSoup as bs
+from pathlib import Path
 
 def scrape_page(url) -> bs:
     """Return BeautifulSoup object from given url address.
@@ -17,14 +19,16 @@ def scrape_page(url) -> bs:
 def select_attributes(tr) -> dict:
     """Select attributes from each line."""
     return {
-        "kod_obce": tr[0].get_text(),
-        "nazev_obce": tr[1].get_text(),
+        "code": tr[0].get_text(), 
+        "location": tr[1].get_text(),
     }
 
-def select_data(soup: bs) -> list:
-    """Return a list of numbers and names of municipalities.
+def get_district_data(soup: bs) -> list:
+    """Return a list of dictionaries with code and name of location
+    from district page.
     Parameters:
-    - soup: bs object"""
+    - soup: bs object representing district page"""
+
     tables = soup.find_all("table", {"class": "table"})
     results = list()
     for table in tables:
@@ -37,8 +41,8 @@ def select_data(soup: bs) -> list:
         ]
     return results
     
-def get_subpage_urls(url, soup) -> list:
-    """Returns a list of strings with subpage url addresses.
+def get_subpage_urls(url: str, soup: bs) -> list:
+    """Returns a list of subpage url addresses.
     Parameters:
     - soup: bs object with url addresses"""
 
@@ -51,27 +55,81 @@ def get_subpage_urls(url, soup) -> list:
             subpages.append(full_url)
     return subpages         
 
-def scrape_subpage(subpages) -> bs:
-    """Scrape all links from given list.
+def scrape_subpages(subpages: list) -> list:
+    """Scrape all links from given list. Return a list with
+    data from all links.
     Parameters:
-    - subpage_urls: list of url links to scrape"""
+    - subpages: list of url links to scrape"""
 
     subpage_soup = list()
     for url in subpages:
         subpage_soup.append(scrape_page(url))
     return subpage_soup    
         
- 
+def select_subpage_attributes(tr) -> dict:
+    """Select attributes from each line."""
+    return {
+        "kod_obce": tr[0].get_text(), 
+        "nazev_obce": tr[1].get_text(),
+    }
+
+def get_first_table_data(subpage_soup: bs) -> list:
+    """Return a count of voters in a list, envelopes issued and 
+    valid votes.
+    Parameters:
+    - subpage_soup: bs object representing the subpage """
+    
+    volici = subpage_soup.find(attrs={"headers": "sa2"})
+    obalky = subpage_soup.find(attrs={"headers": "sa3"})
+    platne_hlasy = subpage_soup.find(attrs={"headers":"sa6"})
+    return [volici, obalky, platne_hlasy]
+
+def select_subpage_data(subpage_soup: bs) -> list:
+    """Return a list of voters, envelopes issued, valid votes
+    and parties running for election (with number of votes).
+    Parameters:
+    - subpage_soup: bs object representing the subpage"""
+    
+    tables = subpage_soup.find_all("table", {"class": "table"})
+    results = list()
+    for table in tables:
+        all_tr = table.find_all("tr")
+        for tr in all_tr[3:]:
+            results.append(select_subpage_attributes(tr.find_all("td")))
+    results = [
+        item for item in results
+        if any(value != "-" for value in item.values())
+        ]
+    return results 
+
+def write_to_csv(data, output_file):
+    """Write the selected data to a CSV file.
+    Parameters:
+    - data: list of lists containing the selected data.
+    - output_file: name of the CSV file to create."""
+    path = Path(output_file)
+    with path.open('w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(data[0].keys())
+        for item in data:
+            writer.writerow(item.values())
+
+
 
 
 
 def main():
     url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2102"
     soup = scrape_page(url)
-    results = select_data(soup)
-    subpages = get_subpage_urls(url, soup)
-    subpage_soup = scrape_subpage(subpages)
-    subpage_results = select_data(subpage_soup)
+    results = get_district_data(soup) #list se slovniky pro vsechny obce na strance (kod a nazev obce)
+    # subpages = get_subpage_urls(url, soup)
+    # subpage_soup = scrape_subpage(subpages)
+    # subpage_first_table = get_first_table_data(subpage_soup)
+    # subpage_results = select_subpage_data(subpage_soup) # v select_data upravit použití 
+    #funkce select_attributes - budou tam jiné atributy než v main page 
+   
+    write_to_csv(results, "selected_data.csv")
+    #-> zadat jako parametry
     # save_to_csv(results, output_file)
 
 if __name__ == "__main__":
